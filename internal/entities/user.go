6b034986d54d3c89bb40aecb5178514c705003ca
devGabrielb <dev.gabrielb1@gmail.com>
2024-01-02 16:26:07 -0300

    feat: add user entity

---
+++ internal/entities/user.go
@@ -0,0 +1,11 @@
+package entities
+
+type User struct {
+	Profile_picture string
+	Name            string
+	Email           string
+	Password        string
+	PhoneNumber     string
+	Location        string
+	Id              int
+}
