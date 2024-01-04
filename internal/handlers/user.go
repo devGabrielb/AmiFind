3af9e0675d5e25e0a5459e0231919b1f676296d9
devGabrielb <dev.gabrielb1@gmail.com>
2024-01-03 22:43:03 -0300

    refactor: improvement login and register handler
    - return a correct status code
    - improvement the error handling

--- internal/handlers/user.go
+++ internal/handlers/user.go
@@ -1,9 +1,15 @@
 package handlers
 
 import (
+	"strconv"
+
 	"github.com/devGabrielb/AmiFind/internal/entities"
 	"github.com/devGabrielb/AmiFind/internal/repositories"
+	"github.com/devGabrielb/AmiFind/internal/services"
+	"github.com/devGabrielb/AmiFind/pkg/env"
 	"github.com/gofiber/fiber/v2"
+	"github.com/sirupsen/logrus"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type UserRegisterRequest struct {
@@ -18,6 +24,12 @@ type UserRegisterRequest struct {
 type LoginRequest struct {
 	Email    string `json:"email" validate:"required,max=24"`
 	Password string `json:"password" validate:"required,max=12"`
+}
+
+type LoginResponse struct {
+	Email string `json:"email,omitempty"`
+	Token string `json:"token,omitempty"`
+	Id    int    `json:"id,omitempty"`
 }
 
 func (u *UserRegisterRequest) Validate() error {
@@ -38,16 +50,21 @@ func (u *userHandler) Register(c *fiber.Ctx) error {
 	if err := c.BodyParser(&userRequest); err != nil {
 		return err
 	}
+	pass, err := bcrypt.GenerateFromPassword([]byte(userRequest.Password), 5)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"Message": "Erro ao criptografar senha"})
+	}
+
 	user := entities.User{
 		Profile_picture: userRequest.Profile_picture_url,
 		Name:            userRequest.Name,
 		Email:           userRequest.Email,
-		Password:        userRequest.Password,
+		Password:        string(pass),
 		PhoneNumber:     userRequest.PhoneNumber,
 		Location:        userRequest.Location,
 	}
-	err := u.repo.Create(c.Context(), user)
-	if err != nil {
+	if err := u.repo.Create(c.Context(), user); err != nil {
+		logrus.WithField("error", err.Error()).Error("error while request user by email")
 		return err
 	}
 
@@ -60,10 +77,28 @@ func (u *userHandler) Login(c *fiber.Ctx) error {
 		return err
 	}
 
-	_, err := u.repo.FindByEmail(c.Context(), loginRequest.Email)
+	user, err := u.repo.FindByEmail(c.Context(), loginRequest.Email)
 	if err != nil {
-		return err
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"message": "user not found"})
 	}
 
-	return nil
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(loginRequest.Password)); err != nil {
+		return c.Status(400).JSON(fiber.Map{
+			"error": err.Error(),
+		})
+	}
+
+	env, err := env.TryGetEnv("SECRET_KEY")
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"message": err.Error()})
+	}
+
+	t := services.NewToken(env)
+
+	token, err := t.GenerateToken(strconv.Itoa(user.Id))
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"message": err.Error()})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(LoginResponse{Id: user.Id, Email: user.Email, Token: token})
 }
