2ba1812c3ee21c0bff22d78b726162cd48952ab0
devGabrielb <dev.gabrielb1@gmail.com>
2024-01-05 22:05:12 -0300

    refactor: change the dtos and standardize the HTTP response

--- internal/dtos/user.go
+++ internal/dtos/user.go
@@ -1,16 +1,18 @@
 package dtos
 
-type RegisterRequest struct {
-	Profile_picture_url string `json:"profilePictureUrl" validate:"required,max=255"`
-	Name                string `json:"name" validate:"required,max=24"`
-	Email               string `json:"email" validate:"required,max=24"`
-	Password            string `json:"password" validate:"required,max=12"`
-	PhoneNumber         string `json:"phoneNumber" validate:"required,max=20"`
-	Location            string `json:"location" validate:"required,max=255"`
-}
+import (
+	"github.com/go-playground/validator/v10"
+)
 
-func (u *RegisterRequest) Validate() error {
-	return nil
+var v *validator.Validate
+
+type RegisterRequest struct {
+	Profile_picture string `json:"profile_picture" validate:"required,http_url"`
+	Name            string `json:"name" validate:"required,max=24"`
+	Email           string `json:"email" validate:"email,required,max=24"`
+	Password        string `json:"password" validate:"required,max=12"`
+	PhoneNumber     string `json:"phoneNumber" validate:"e164,required,max=20"`
+	Location        string `json:"location" validate:"required,max=255"`
 }
 
 type LoginRequest struct {
@@ -26,4 +28,15 @@ type LoginResponse struct {
 	Email string `json:"email,omitempty"`
 	Token string `json:"token,omitempty"`
 	Id    int    `json:"id,omitempty"`
+}
+
+func Validate(model interface{}) error {
+
+	v = validator.New(validator.WithRequiredStructEnabled())
+	err := v.Struct(model)
+	if err != nil {
+		return err
+
+	}
+	return nil
 }
