d6499136a8b9b1c95fb255b30ee3c8460c6dd02c
devGabrielb <dev.gabrielb1@gmail.com>
2024-01-08 00:52:21 -0300

    refactor: change name of structs and interfaces

--- internal/repositories/user.go
+++ internal/repositories/user.go
@@ -7,20 +7,20 @@ import (
 	"github.com/devGabrielb/AmiFind/internal/entities"
 )
 
-type Repository interface {
+type UserRepository interface {
 	FindByEmail(ctx context.Context, email string) (entities.User, error)
 	Create(ctx context.Context, user entities.User) (int64, error)
 }
 
-type repository struct {
+type userRepository struct {
 	db *sql.DB
 }
 
-func NewRepository(db *sql.DB) Repository {
-	return &repository{db: db}
+func NewRepository(db *sql.DB) UserRepository {
+	return &userRepository{db: db}
 }
 
-func (r *repository) FindByEmail(ctx context.Context, email string) (entities.User, error) {
+func (r *userRepository) FindByEmail(ctx context.Context, email string) (entities.User, error) {
 	query := "SELECT id,profile_picture,name,email,password,phoneNumber,location FROM users WHERE email=?;"
 	row := r.db.QueryRowContext(ctx, query, email)
 	user := entities.User{}
@@ -33,7 +33,7 @@ func (r *repository) FindByEmail(ctx context.Context, email string) (entities.Us
 	return user, nil
 }
 
-func (r *repository) Create(ctx context.Context, user entities.User) (int64, error) {
+func (r *userRepository) Create(ctx context.Context, user entities.User) (int64, error) {
 	u, err := r.db.ExecContext(
 		ctx,
 		"INSERT INTO users(profile_picture,name, email, password, phoneNumber, location) VALUES (?,?,?,?,?,?);",
